5354d866561cc394197868d08aa4a95c2a318d7e
suudupa <suhas.udupa@yahoo.ca>
2020-03-21 12:57:30 -0400

    Added new constants

--- app/src/main/java/com/suudupa/coronavirustracker/utility/Resources.java
+++ app/src/main/java/com/suudupa/coronavirustracker/utility/Resources.java
@@ -7,6 +7,10 @@ public class Resources {
     public static final String GLOBAL = "Global";
     public static final String REGION_URL = "https://www.worldometers.info/coronavirus/#countries";
     public static final String API_KEY = "8adc6c6586d44ce49ee3a170649ec33b";
+    public static final String KEYWORD_1 = "coronavirus";
+    public static final String KEYWORD_2 = "covid-19";
+    public static final String OR = " OR ";
+    public static final String AND = " AND ";
 
     public static final int CASES_COL = 1;
     public static final int DEATHS_COL = 3;
