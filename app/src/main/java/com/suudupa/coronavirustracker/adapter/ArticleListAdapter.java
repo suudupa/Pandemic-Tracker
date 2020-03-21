f59be857c351d43b55123dc06284500d7ca4476c
suudupa <suhas.udupa@yahoo.ca>
2020-03-21 01:26:36 -0400

    Added adapter for recycleView

---
+++ app/src/main/java/com/suudupa/coronavirustracker/adapter/ArticleListAdapter.java
@@ -0,0 +1,126 @@
+package com.suudupa.coronavirustracker.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
+import com.suudupa.coronavirustracker.R;
+import com.suudupa.coronavirustracker.model.Article;
+import com.suudupa.coronavirustracker.utility.Utils;
+
+import java.util.List;
+
+public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<Article> articles;
+    private OnItemClickListener onItemClickListener;
+
+    public ArticleListAdapter(List<Article> articles, Context context) {
+        this.articles = articles;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.article_list, parent, false);
+        return new MyViewHolder(view, onItemClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holders, int position) {
+
+        final MyViewHolder holder = holders;
+        Article article = articles.get(position);
+
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.diskCacheStrategy(DiskCacheStrategy.ALL);
+        requestOptions.centerCrop();
+
+        Glide.with(context)
+                .load(article.getUrlToImage())
+                .apply(requestOptions)
+                .listener(new RequestListener<Drawable>() {
+
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(holder.imageView);
+
+        holder.title.setText(article.getTitle());
+        holder.description.setText(article.getDescription());
+        holder.source.setText(article.getSource().getName());
+        holder.time.setText(" \u2022 " + Utils.formatDateTime(article.getPublishedAt()));
+        holder.publishedAt.setText(Utils.formatDate(article.getPublishedAt()));
+        holder.author.setText(article.getAuthor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener){
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView title, description, author, publishedAt, source, time;
+        ImageView imageView;
+        ProgressBar progressBar;
+        OnItemClickListener onItemClickListener;
+
+        public MyViewHolder(View listView, OnItemClickListener onItemClickListener) {
+
+            super(listView);
+            itemView.setOnClickListener(this);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+            author = itemView.findViewById(R.id.author);
+            publishedAt = itemView.findViewById(R.id.publishedAt);
+            source = itemView.findViewById(R.id.source);
+            time = itemView.findViewById(R.id.time);
+            imageView = itemView.findViewById(R.id.img);
+            progressBar = itemView.findViewById(R.id.prograss_load_photo);
+            this.onItemClickListener = onItemClickListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            onItemClickListener.onItemClick(v, getAdapterPosition());
+        }
+    }
+}
