a32dce0d47b18cb7228ee2dce752abab20dc1497
suudupa <suhas.udupa@yahoo.ca>
2020-03-20 22:01:09 -0400

    Renamed package

---
+++ app/src/main/java/com/suudupa/coronavirustracker/utility/Resources.java
@@ -0,0 +1,11 @@
+package com.suudupa.coronavirustracker.utility;
+
+public class Resources {
+
+    public static final String HOMEPAGE_URL = "https://www.worldometers.info/coronavirus/";
+    public static final String OUTBREAK_DATA = ".maincounter-number";
+    public static final String REGION_URL = "https://www.worldometers.info/coronavirus/#countries";
+    public static final String API_KEY = "8adc6c6586d44ce49ee3a170649ec33b";
+    public static final String KEYWORD = "coronavirus";
+    public static final String SORT_BY = "relevancy";
+}
