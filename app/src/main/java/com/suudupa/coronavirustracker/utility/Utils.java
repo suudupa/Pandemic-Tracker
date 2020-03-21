b203122a3d7d3da1f4b69467d1e4d7388e320ee2
suudupa <suhas.udupa@yahoo.ca>
2020-03-21 01:27:16 -0400

    Added utils class

---
+++ app/src/main/java/com/suudupa/coronavirustracker/utility/Utils.java
@@ -0,0 +1,61 @@
+package com.suudupa.coronavirustracker.utility;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+
+import org.ocpsoft.prettytime.PrettyTime;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+
+public class Utils {
+
+    public static ColorDrawable[] vibrantLightColorList =
+            {
+                    new ColorDrawable(Color.parseColor("#ffeead")),
+                    new ColorDrawable(Color.parseColor("#93cfb3")),
+                    new ColorDrawable(Color.parseColor("#fd7a7a")),
+                    new ColorDrawable(Color.parseColor("#faca5f")),
+                    new ColorDrawable(Color.parseColor("#1ba798")),
+                    new ColorDrawable(Color.parseColor("#6aa9ae")),
+                    new ColorDrawable(Color.parseColor("#ffbf27")),
+                    new ColorDrawable(Color.parseColor("#d93947"))
+            };
+
+    public static ColorDrawable getRandomColorDrawable() {
+        int index = new Random().nextInt(vibrantLightColorList.length);
+        return vibrantLightColorList[index];
+    }
+
+    public static String getCountry() {
+        Locale locale = Locale.getDefault();
+        return locale.getCountry().toLowerCase();
+    }
+
+    public static String formatDate (String dateString) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("E, d MMM yyyy", new Locale(getCountry()));
+        try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(dateString);
+            return dateFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static String formatDateTime (String dateString) {
+        PrettyTime p = new PrettyTime(new Locale(getCountry()));
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+            Date date = simpleDateFormat.parse(dateString);
+            return p.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
