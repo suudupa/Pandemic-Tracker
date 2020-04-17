446b277564bc811c3dec70df75ce83bd121a9cd6
suudupa <suhas.udupa@yahoo.ca>
2020-04-16 21:23:27 -0400

    Now pulling data from new website

---
+++ app/src/main/java/com/suudupa/coronavirustracker/asyncTask/JsonResponse.java
@@ -0,0 +1,68 @@
+package com.suudupa.coronavirustracker.asyncTask;
+
+import android.os.AsyncTask;
+
+import com.suudupa.coronavirustracker.activity.MainActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class JsonResponse extends AsyncTask<Object, Void, Void> {
+
+    @Override
+    protected Void doInBackground(Object... params) {
+
+        String link = (String) params[0];
+        MainActivity mainActivityContext = (MainActivity) params[1];
+
+        HttpURLConnection connection = null;
+        URL url = null;
+        String json = "";
+
+        try {
+            url = new URL(link);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            assert url != null;
+            connection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(connection.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "utf-8"), 8);
+            json = getJson(reader);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            assert connection != null;
+            connection.disconnect();
+        }
+
+        try {
+            mainActivityContext.jsonResponse = new JSONObject(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private String getJson(BufferedReader reader) throws IOException {
+        StringBuilder sBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sBuilder.append(line + "\n");
+        }
+        return sBuilder.toString();
+    }
+}
