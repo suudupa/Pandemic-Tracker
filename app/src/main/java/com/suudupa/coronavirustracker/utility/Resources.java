dafeaccd897f707cf82d4047b5386adb7c96f00e
suudupa <suhas.udupa@mail.mcgill.ca>
2020-05-12 21:10:23 -0400

    Added new API keys

--- app/src/main/java/com/suudupa/coronavirustracker/utility/Resources.java
+++ app/src/main/java/com/suudupa/coronavirustracker/utility/Resources.java
@@ -14,12 +14,14 @@ public class Resources {
     public static final String NO_CONNECTION = "No Internet Connection";
     public static final String NO_CONNECTION_ACTION = "DISMISS";
     public static final String NEWS_API_URL = "https://newsapi.org/v2/";
-    private static final String API_KEY_1 = "8adc6c6586d44ce49ee3a170649ec33b";
+    private static final String API_KEY_1 = "f2f3b9360a53427c9bd42132b20efe07";
     private static final String API_KEY_2 = "66fbfc3fa8a44467ae93c9d59d8edf7e";
     private static final String API_KEY_3 = "2ababc39711d4f3f9825b7a310d887f9";
     private static final String API_KEY_4 = "598f676618b742b6ac5ed52a4db989e4";
     private static final String API_KEY_5 = "1e2de1f8c1fc40868687a5b955896bfd";
-    static final String[] API_KEYS = {API_KEY_1, API_KEY_2, API_KEY_3, API_KEY_4, API_KEY_5};
+    private static final String API_KEY_6 = "f22d18235e724ca2893e202077ff79c1";
+    private static final String API_KEY_7 = "0ad3f130044e42f6b247149fd3b5b6c7";
+    static final String[] API_KEYS = {API_KEY_1, API_KEY_2, API_KEY_3, API_KEY_4, API_KEY_5, API_KEY_6, API_KEY_7};
     public static final String KEYWORD_1 = "coronavirus";
     public static final String KEYWORD_2 = "covid-19";
     public static final String OR_OP = " OR ";
